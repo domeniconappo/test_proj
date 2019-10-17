f57c64f101ec88d5d50dfe91ea9bc5180f1b52f5
Domenico Nappo <domenico.nappo@gmail.com>
2019-10-17 11:47:40 +0200

    not important commit

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import os
 filename = 'text.txt'
 
 if __name__ == '__main__':
-    with os.open(filename, 'w') as f:
-        f.write('b')
+    with os.open(filename, 'w') as fh:
+        fh.write('b')
