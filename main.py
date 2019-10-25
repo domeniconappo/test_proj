b2fc422eda1c93d21e18d8e45572dddcde63499d
Domenico Nappo <domenico.nappo@gmail.com>
2019-10-25 11:39:39 +0200

    changed content

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ if __name__ == '__main__':
     if not os.path.exists('./output'):
         os.mkdir('./output')
     with open('./output/text.txt', 'w') as fh:
-        fh.write('c')
+        fh.write('D')
