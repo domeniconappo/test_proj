1d8b4e0870630b8a0e6c7251854dd412656eabb7
Domenico Nappo <domenico.nappo@gmail.com>
2019-10-25 11:38:32 +0200

    with idiom

--- main.py
+++ main.py
@@ -4,6 +4,5 @@ import os
 if __name__ == '__main__':
     if not os.path.exists('./output'):
         os.mkdir('./output')
-    fh = open('./output/text.txt', 'w')
-    fh.write('c')
-    fh.close()
+    with open('./output/text.txt', 'w') as fh:
+        fh.write('c')
